c3d181b3edab283d492afbf210519fc0a6a1e492
Asad Rao <63543233+asadrao98@users.noreply.github.com>
2020-10-18 20:35:03 +0500

    initial commit

---
+++ index.js
@@ -0,0 +1,88 @@
+// Calculator
+
+let runningTotal = 0;
+let buffer = "0";
+let previousOperator;
+const screen = document.querySelector(".screen");
+let btndel = document.querySelector(".btndel");
+
+document.querySelector('.calc-button').addEventListener('click' , function(){
+    buttonClick(event.target.innerText);
+  
+});
+
+function buttonClick(value){
+    if(isNaN(parseInt(value)) ){
+        handleSymbol(value);
+    }
+    else{
+        handleNumber(value);
+    }
+    rerender();
+}
+
+function handleNumber(value){
+    if(buffer === "0"){
+        buffer = value;
+    }
+    else{
+        buffer += value;
+    }
+    
+}
+
+function handleSymbol(value){
+    switch(value){
+        case 'C':
+            buffer = 0;
+            runningTotal = 0;
+            previousOperator = null;
+            break;
+        case "=":
+            if(previousOperator === null){
+                return;
+            }
+            flushOperation(parseInt(buffer));
+            previousOperator = null;
+            buffer = "" + runningTotal;
+            break;
+            default:
+                handleMath(value);
+
+    }
+}
+
+btndel.addEventListener('click',(e)=>{
+    buffer = buffer.substring(0,buffer.length-1);
+});
+
+function handleMath(value){
+    const intBuffer = parseInt(buffer);
+    if(runningTotal === 0){
+        runningTotal = intBuffer;
+    }
+    else{
+        flushOperation(intBuffer);
+    }
+    previousOperator = value;
+    buffer = "0";
+}
+
+function flushOperation(intBuffer){    
+    if(previousOperator === "+"){
+        runningTotal += intBuffer;
+    }
+    else if(previousOperator === "-"){
+        runningTotal -= intBuffer;
+    }
+    else if(previousOperator === "/"){
+        runningTotal /= intBuffer;
+    }
+    else{
+        runningTotal *= intBuffer;
+    }
+}
+
+function rerender(){
+    screen.innerText = buffer; 
+}
